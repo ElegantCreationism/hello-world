df42c522ef1bd5f3960dbb64afe8288776e2f6ef
ElegantCreationism <m.j.hamilton@outlook.com>
2018-06-01 10:21:16 +0100

    wip

--- hello_test.go
+++ hello_test.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"testing"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSayHello(t *testing.T) {
 	expectedGreeting := "hello, john"
 	greeting := SayHello("john")
 
-	assert.Equal(t, expectedGreeting, greeting)
+
+	if expectedGreeting != greeting {
+		t.Fail()
+	}
+
 }
